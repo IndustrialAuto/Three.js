5fc34a52d56b597f28f81f6855298db6334153bc
Aviral Singh <aviral.singh68@gmail.com>
2022-08-17 21:01:39 +0530

    commit

--- Experience/Camera.js
+++ Experience/Camera.js
@@ -6,6 +6,30 @@ export default class Camera{
         this.sizes = this.experience.sizes;
         this.scene = this.experience.scene;
         this.canvas = this.experience.canvas;
-        console.log(this.experience, this.sizes, this.scene, this.canvas);
+       
+        this.createPerspectiveCamera();
+        this.createOrthographicCamera();
+    }
+    createPerspectiveCamera(){
+        this.perspectiveCamera = new THREE.PerspectiveCamera(
+            35,
+            this.sizes.aspect,
+            0.1,
+            1000
+        );
+        this.scene.add(this.perspectiveCamera)
+    }
+    createOrthographicCamera(){
+        this.frustrum = 5;
+        this.orthographicCamera = new THREE.OrthographicCamera(
+            (-this.sizes.aspect * this.sizes.frustrum) / 2,
+            (this.sizes.aspect * this.frustrum) / 2,
+            this.sizes.frustrum / 2,
+            -this.sizes.frustrum / 2,
+            -100,
+            100
+           
+        );
+        this.scene.add(this.orthographicCamera)
     }
 }
