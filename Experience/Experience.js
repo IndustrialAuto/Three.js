5766994475f6b61cfc6181a8b0f2e4cc6be2546b
Aviral Singh <aviral.singh68@gmail.com>
2022-08-17 23:23:33 +0530

    commit

--- Experience/Experience.js
+++ Experience/Experience.js
@@ -1,6 +1,7 @@
 import * as THREE from "three";
 
 import Sizes from "./Utils/Sizes.js";
+import Time from "./Utils/Time";
 
 import Camera from "./Camera.js";
 import Renderer from "./Renderer.js";
@@ -16,6 +17,7 @@ export default class Experience {
       	this.canvas = canvas;
 		this.scene = new THREE.Scene();
 		this.sizes = new Sizes();
+		this.time = new Time();
 		this.camera = new Camera();
 		this.renderer = new Renderer();
 
