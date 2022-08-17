1a6ee735cb5d1a223b8f772492b5090c5e37e442
Aviral Singh <aviral.singh68@gmail.com>
2022-08-17 18:29:16 +0530

    commit

--- Experience/Camera.js
+++ Experience/Camera.js
@@ -3,9 +3,9 @@ import Experience from "./Experience";
 export default class Camera{
     constructor(){
         this.experience = new Experience();
-        this.sizes = this.experience.sizes();
-        this.scene = this.experience.scene();
-        this.canvas = this.experience.canvas();
+        this.sizes = this.experience.sizes;
+        this.scene = this.experience.scene;
+        this.canvas = this.experience.canvas;
         console.log(this.experience, this.sizes, this.scene, this.canvas);
     }
 }
