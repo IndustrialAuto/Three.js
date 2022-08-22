019afcaed5607a9b7d4822994fa05c094c1fca92
Aviral Singh <aviral.singh68@gmail.com>
2022-08-22 10:00:30 +0530

    commit

--- Experience/Renderer.js
+++ Experience/Renderer.js
@@ -30,7 +30,7 @@ export default class Renderer{
         this.renderer.toneMappingExposure = 1.75;
         this.renderer.shadowMap.enabled = true;
         this.renderer.shadowMap.type = THREE.PCFShadowMap;
-        this.renderer.setSize(700, 600);
+        this.renderer.setSize(this.sizes.width, this.sizes.height);
         this.renderer.setPixelRatio(this.sizes.pixelRatio);
 
         
