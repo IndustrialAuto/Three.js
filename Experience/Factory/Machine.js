4850f42fe871a549077a04de3db2f4473ea99575
Aviral Singh <aviral.singh68@gmail.com>
2022-08-22 23:22:05 +0530

    commit

--- Experience/Factory/Machine.js
+++ Experience/Factory/Machine.js
@@ -8,13 +8,17 @@ export default class Machine{
         this.scene = this.experience.scene;
         this.resources = this.experience.resources;
         this.machine = this.resources.items.machine;
+        this.actualMachine = this.machine.scene;
         console.log(this.machine);
 
-        
+        this.setModel();
+
         //console.log(this.camera, this.camera.perspectiveCamera, this.canvas)
         
+    }
 
-        
+    setModel(){
+        this.scene.add(this.actualMachine);
     }
 
     
