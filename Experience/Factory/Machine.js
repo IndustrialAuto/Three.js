b18f0f44051bca828fde6c30d30c7064908f7611
Aviral Singh <aviral.singh68@gmail.com>
2022-08-23 01:11:06 +0530

    commit

--- Experience/Factory/Machine.js
+++ Experience/Factory/Machine.js
@@ -19,7 +19,7 @@ export default class Machine{
 
     setModel(){
         this.scene.add(this.actualMachine);
-        this.actualMachine.scale.set(1, 1);  //to scale the object size
+        this.actualMachine.scale.set(1,1,1);  //to scale the object size
     }
 
     
