97e3c9ec4893866d3edf0af676328dbf03053433
Aviral Singh <aviral.singh68@gmail.com>
2022-08-22 21:18:31 +0530

    commit

--- Experience/Utils/Resources.js
+++ Experience/Utils/Resources.js
@@ -1,5 +1,7 @@
 import {EventEmitter} from "events";
 import Experience from "../Experience.js"
+import {GLTFLoader} from "three/examples/jsm/loaders/GLTFLoader.js"
+import {DRACOLoader} from "three/examples/jsm/loaders/DRACOLoader.js"
 
 
 export default class Resources extends EventEmitter{
@@ -9,9 +11,59 @@ export default class Resources extends EventEmitter{
         this.renerer = this.experience.renderer;
 
         this.assets = assets;
-        
-        console.log(this.assets);
+
+        //console.log(this.assets);
+
+        this.items = {};
+        this.queue = this.assets.length;
+        this.loaded = 0;
+
+        this.setLoaders();
+        this.startLoading();
 
       
         }
+    
+    setLoaders(){
+        this.loaders = {};
+        this.loaders.gltfLoader = new GLTFLoader();
+        this.loaders.dracoLoader = new DRACOLoader();
+        this.loaders.dracpLoader.setDecoderPath("/draco");
+        this.loaders.gltfLoader.setDracoLoader(this.loaders.dracoLoader);
+
+    }
+
+    startLoading() {
+        for(const asset of this.assets){
+            if(asset.type === "glbModel"){
+                this.loaders.gltfLoader.load(asset.path, (file) => {
+                    this.signleAssetLoaded(asset,file);
+                });
+            }
+            else if (asset.type === "videoTexture") {
+                this.video = {};
+                this.videoTexture = {};
+
+                this.video[assets.name] = document.createElement("video");
+                this.video[assets.name].src = asset.path;
+                this.video[assets.name].playsInline = true;
+                this.video[assets.name].autoplay = true;
+                this.video[assets.name].play();
+
+                this.videoTexture[asset.name].flipy = true;
+                this.vodeoTexture[asset.name].minFilter = THREE.NearestFilter;
+                this.videoTexture[asset.name].anyFilter = THREE.NearestFilter;
+                this.videoTexture[asset.name].generateMIomaps = false;
+                this.videoTexture[asset.name].encoding = THREE.sRGBEncoding;
+
+            }
+        }
+
+    }
+
+    signleAssetLoaded(asset, file) {
+        this.items[asset.name] = file;
+        this.loaded++;
+    }
+
 } 
